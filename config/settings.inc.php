475f63430834ec4aebafe475ef5384b9ce3faa84
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-05-26 08:24:44 +0200

    Remove settings from gitignore to allow login by tierce person

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'scotchbox');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'presta_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'o8EScKTRWXI3PnnyVxBs6BVCg18N1myLazvWMROJTnfm7T9DRYjBy0cj');
+define('_COOKIE_IV_', 'Jb9G7OIk');
+define('_PS_CREATION_DATE_', '2016-04-11');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.4');
+define('_RIJNDAEL_KEY_', '1yBFnScp5Rjc0vOq1mfxWDjFB9w2L44h');
+define('_RIJNDAEL_IV_', '2jnUtJeWyJjdtwrw2Id9VA==');
